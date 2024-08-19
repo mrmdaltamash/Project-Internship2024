647d359d6f3cfcddbd45ceaf0509ed31251dea86
mrmdaltamash <141748443+mrmdaltamash@users.noreply.github.com>
2024-08-19 23:37:22 +0530

    Project name:- ASUS Technoligies

---
+++ scripts.js
@@ -0,0 +1,19 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
+
+function showSlides() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 5000); // Change slide every 5 seconds
+}
+
+showSlides();
